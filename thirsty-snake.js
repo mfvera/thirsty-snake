eb8d09058a208a4d3ce8a21cf09c0643536b9037
mfvera <mfvera@rams.colostate.edu>
2020-06-14 13:48:45 -0600

    Refactored word library

--- thirsty-snake.js
+++ thirsty-snake.js
@@ -2,33 +2,21 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const words = require("./words");
+const { getRandomName } = require("./wordLib");
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 client.on('message', message => {
-    if (message.content === "!reroll") {
-        console.log("Randomizing nickname...");
-        message.member.setNickname(getRandomName());
+    // Nickname property only exists on a Guild. DM's exist outside of guilds and kill the bot.
+    if (message.guild !== null) {
+        if (message.content === "!reroll") {
+            console.log("Randomizing nickname...");
+            message.member.setNickname(getRandomName());
+        }
     }
 })
 
-function getRandomName() {
-    const adjective = choice(words.adjectives);
-    const noun = choice(words.nouns);
-    console.log(`adj: ${adjective}, noun: ${noun}`);
-    return capitalizeFirst(adjective) + " " + capitalizeFirst(noun);
-}
-
-// Borrowed from: https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
-function capitalizeFirst(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function choice(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
 
 client.login(process.env.TOKEN);
